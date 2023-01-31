613cf00a9e89c0a27775f55782700f14c1099e97
vishaljothe994 <jothevishal994@gmail.com>
2023-01-31 12:10:34 +0530

    first commit

---
+++ app1/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Student 
+# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+ list_display=('id', 'stuid', 'stuname', 'stuemail', 'stupass')
+
+#admin.site.register(Student)
+
